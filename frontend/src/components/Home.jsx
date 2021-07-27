6b92021ed5110a6d3ec67c59643d2072ed89eb0c
PabloD97 <pablo97758@gmail.com>
2021-07-27 16:35:04 -0300

    feat: Home front incomplete
    - Se agrego el componente Home
    - Se agrego el componente NavBar en el compoente Home

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import NavBar from './NavBar'
+
+//TODO: Mejorar la home para que muestre mas informacion
+const Home = () => {
+    return(
+        <Fragment>
+            <NavBar/>
+            Home
+        </Fragment>
+    )
+}
+
+export default Home;
