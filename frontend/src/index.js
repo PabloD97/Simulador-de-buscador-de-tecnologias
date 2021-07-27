411a63e29049cc887b22a67560a06d8f9c197021
PabloD97 <pablo97758@gmail.com>
2021-07-27 16:21:41 -0300

    feat: Arrancodo con el Front
    - Se agrego el archivo index.js configurado para que cargue las rutas

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Rutas from './rutas/Rutas'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Rutas />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
