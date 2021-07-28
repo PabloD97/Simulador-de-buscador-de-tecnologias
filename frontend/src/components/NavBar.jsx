0a3cf6a29f3e4f48f7015c8d755e6147ab880e64
PabloD97 <pablo97758@gmail.com>
2021-07-28 00:08:55 -0300

    feat: NavBar Search
    - Se agrego el campo de busqueda con el boton search

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,23 +1,56 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
-import {Navbar, Container, Nav} from 'react-bootstrap'
+import React, { Fragment, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  Button,
+  FormControl,
+} from "react-bootstrap";
 
 const NavBar = () => {
-    return(
-        <Navbar bg="dark" variant="dark">
-            <Container>
-            <Navbar.Brand>Technologies find</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav"  />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                <Nav.Link href="/home">Home</Nav.Link>
-                <Nav.Link href="/technologies">Technologies</Nav.Link>
+  const [find, setFind] = useState("");
+  const history = useHistory();
 
-                </Nav>
-            </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+  const handleInput = (event) => {
+    setFind(event.target.value);
+    console.log(find);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    history.push(`/search/${find}`);
+  };
+
+
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand>Technologies find</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/technologies">Technologies</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="mr-2 tecnologia"
+              aria-label="Search"
+              value={find}
+              onChange={handleInput}
+            />
+            <Button variant="outline-success" onClick={handleSubmit}>
+              Search
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
